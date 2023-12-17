b6a163decef617c46c4ca2aaec5a0ed7d0052d37
Jahdah Thelen <jahdah.thelen@gmail.com>
2023-12-17 15:16:44 +0100

    added -t option to use test inputs

--- 2023/src/main.py
+++ 2023/src/main.py
@@ -5,17 +5,22 @@ from natsort import natsorted
 
 
 def main():
-    names = sys.argv[1:]
+    opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+    args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+
+    file_extension = ".txt"
+    if "-t" in opts:
+        file_extension = ".test.txt"
     days = metadata.entry_points().select(group="aoc23.days")
     for entry in natsorted(days, key=lambda entry: entry.name):
-        if names and entry.name not in names:
+        if args and entry.name not in args:
             continue
         a = "unknown"
         b = "unknown"
         try:
             day = entry.load()
-            a = day.a(f"{entry.name}a.txt")
-            b = day.b(f"{entry.name}b.txt")
+            a = day.a(f"{entry.name}a{file_extension}")
+            b = day.b(f"{entry.name}b{file_extension}")
         except (ModuleNotFoundError, AttributeError):
             pass
         finally:
