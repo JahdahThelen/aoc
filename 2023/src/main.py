c70f81d8abccbd5d8ac2117fe611c1b8b0d3b184
Jahdah Thelen <jahdah.thelen@gmail.com>
2023-12-17 16:08:18 +0100

    added usage and extended cmd options

--- 2023/src/main.py
+++ 2023/src/main.py
@@ -4,19 +4,30 @@ from importlib import metadata
 from natsort import natsorted
 
 
+USAGE = f"Usage: poetry run {sys.argv[0]} [--help] | [--test] [1 - 25]"
+
 def main():
     opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
     args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
 
+    if "-h" in opts or "--help" in opts:
+        print(USAGE)
+        return
+
     file_extension = ".txt"
-    if "-t" in opts:
+    if "-t" in opts or "--test" in opts:
         file_extension = ".test.txt"
-    days = metadata.entry_points().select(group="aoc23.days")
-    for entry in natsorted(days, key=lambda entry: entry.name):
-        if args and entry.name not in args:
+
+    runAOC(args, file_extension)
+
+def runAOC(days: [str], file_extension: str):
+    available_days = metadata.entry_points().select(group="aoc23.days")
+    for entry in natsorted(available_days, key=lambda entry: entry.name):
+        if days and entry.name not in days:
             continue
         a = "unknown"
         b = "unknown"
+
         try:
             day = entry.load()
             a = day.a(f"{entry.name}a{file_extension}")
@@ -25,6 +36,7 @@ def main():
             pass
         finally:
             print(f"Day {entry.name:>{2}}:   a = {a:<{10}} b = {b:<{10}}")
+
 
 if __name__ == "__main__":
     main()
